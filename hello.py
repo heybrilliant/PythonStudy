d4804711631203695cfe7a83cea44b554394b968
EunKyung Shin <heybrilliante@gmail.com>
2021-10-19 23:30:21 +0900

    211018 파이썬 기초) print 함수와 기타

---
+++ hello.py
@@ -0,0 +1,8 @@
+# 함수 : python이 특정한 기능을 실행하도록 하는 명령
+print('Hello, World!')
+# print() 함수 
+# 괄호 안에 있는 내용을 화면에 출력하는 명령
+# 화면에 표시하고 싶은 글은 따옴표(')로 묶어서 표현
+
+#  ctrl + / : 주석처리 
+#  exit() : powershell 내에서 document 빠져나오는 명령어
