800a93da658db2464665c1ff5a32a76574db5363
EunKyung Shin <heybrilliante@gmail.com>
2021-10-24 23:58:25 +0900

    211024 파이썬 기초_함수 만들기2

---
+++ ProgrammersPythonBasic/PythonBasic4.Function/function2.py
@@ -0,0 +1,20 @@
+#2021.10.24 Progrmmers Python Basic <function2>
+
+def function():
+    print('안녕, 함수!')
+
+function()
+# 함수 생성 후 함수를 선언해줘야 코드가 실행됨
+
+print()
+print()
+print()
+# print()는 빈줄을 출력
+
+# 함수 문법
+"""
+def = define 정의하다
+function = 함수 이름, 사용자가 정의 가능
+() :
+들여쓰기 + 코드 
+"""
