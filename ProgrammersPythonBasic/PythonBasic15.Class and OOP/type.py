7e28a5db08e81865b395ca30c46350826f6a0e67
EunKyung Shin <heybrilliante@gmail.com>
2021-11-24 23:59:31 +0900

    211124 파이썬 기초_자료형 다루기

---
+++ ProgrammersPythonBasic/PythonBasic15.Class and OOP/type.py
@@ -0,0 +1,17 @@
+#2021.11.24 Progrmmers Python Basic <자료형 다루기>
+
+s = "Hello world"
+print(type(s)) # <class 'str'> 출력
+# type(변수)를 입력하면 어떤 자료형인지 출력됨
+f = 3.14
+print(type(f)) # <class 'float'> 출력
+# float = 부동소수점
+i = 42
+print(type(i)) # <class 'int'> 출력
+# int = 정수
+print(42.0) # <class 'float'>
+print(42 == 42.0) #True 출력
+
+
+isinstance(42, int)
+isinstance(42, float)
